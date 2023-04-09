5acde0b7093032e01e56101b86ec8fb9683c7782
hjh <hanjiahongfly@163.com>
2023-04-09 15:02:55 +0800

    子活动信息接受模板页

---
+++ miniprogram/pages/start-activity/start-activity.ts
@@ -0,0 +1,84 @@
+// pages/start-activity/start-activity.ts
+const { get } = require('../../utils/request')
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    id: 0,
+    activities: [], // Set the URL for the cross-origin page
+    buttonText: '开始' // Set the text for the button
+  },
+  async getActivityrInfo() {
+    const url = '/api/activity/info'
+    let that = this;
+    const query = {
+      id: that.data.id
+    }
+    const res = await get(url, query)
+    console.log('data===', res)
+    this.setData({ activities: res.data.activities })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      id: parseInt(options.id)
+    });
+    this.getActivityrInfo();
+  },
+  handleButtonClick: function () {
+    // Handle button click event
+    console.log('Button clicked')
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
