551f0ef23490aa1282c1b446cc19aa2b05c8e257
hjh <hanjiahongfly@163.com>
2023-04-20 04:35:36 +0800

    请灯页面样式

--- miniprogram/pages/qingdeng/qingdeng.ts
+++ miniprogram/pages/qingdeng/qingdeng.ts
@@ -11,27 +11,51 @@ Page({
       {
         name: '紫薇',
         value: 'ziwei'
-      }, 
+      },
       {
         name: '白虎',
         value: 'baihu'
-       },
-       { 
-         name: '青龙',
-         value: 'qinglong'
-        }, 
-        { 
-          name: '朱雀',
-          value: 'zhuque'
-         },
-         {
-           name: '玄武',
-           value: 'xuanwu'
-         }
-        ],
+      },
+      {
+        name: '青龙',
+        value: 'qinglong'
+      },
+      {
+        name: '朱雀',
+        value: 'zhuque'
+      },
+      {
+        name: '玄武',
+        value: 'xuanwu'
+      }
+    ],
     locationIndex: 0,
     locationName: "紫薇",
     amount: 0,
+    list: [{
+      stepInfo: [{
+        status: 1,
+        pro_name: '选择星类'
+      },
+      {
+        status: 0,
+        pro_name: '填写信息'
+      },
+      {
+        status: 0,
+        pro_name: '支付'
+      },
+      
+        // {
+        //   status: 0,
+        //   pro_name: '退款失败'
+        // },
+      ],
+      sel_icon: "../../icons/finished-progress.png",
+      per: 0,
+      pro_color: '#cfa671'
+    }],
+
     msg: ""
   },
 
@@ -82,7 +106,7 @@ Page({
       amount: +event.detail.value
     })
   },
-  
+
 
   /**
    * Lifecycle function--Called when page is initially rendered
@@ -95,7 +119,16 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    let {
+      list
+    } = this.data;
+    list.forEach(item => {
+      let onePer = item.stepInfo.filter(i0 => i0.status == 1).length;
+      item.per = 100 / (item.stepInfo.length - 1) * (onePer - 1)
+    })
+    this.setData({
+      list
+    })
   },
 
   /**
